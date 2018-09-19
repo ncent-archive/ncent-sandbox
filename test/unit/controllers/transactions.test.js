20f914b9c3659af19bdbc45c280c9e81922d1de9
Dustin Pearson <dustinparkerpearson@gmail.com>
2018-09-18 17:28:59 -0700

    Add test for provenance FIFO

--- test/unit/controllers/transactions.test.js
+++ test/unit/controllers/transactions.test.js
@@ -137,88 +137,91 @@ describe('transactions Controller', () => {
   })
 
 
-  // describe('provenanceChain', () => {
-  //   it('returns an accurate provenance chain', async (done) => {
-  //       const tHandler = async (transactionObject) => {
-  //         const newTransaction = transactionObject.transaction.txn;
-  //         const newReceiverKeypair = transactionObject.receiverKeypair;
-  //         const tests = (provenanceChain) => {
-  //           expect(provenanceChain.length).toBe(2);
-  //           const firstTransaction = provenanceChain[0];
-  //           const secondTransaction = provenanceChain[1];
-  //           expect(secondTransaction.uuid).toBe(newTransaction.uuid);
-  //           expect(firstTransaction.amount).toBe(AMOUNT);
-  //           expect(secondTransaction.amount).toBe(AMOUNT);
-  //           expect(firstTransaction.fromAddress).toBe(walletOwnerKeypair.publicKey());
-  //           expect(firstTransaction.toAddress).toBe(receiverKeypair.publicKey());
-  //           expect(secondTransaction.fromAddress).toBe(receiverKeypair.publicKey());
-  //           expect(secondTransaction.toAddress).toBe(newReceiverKeypair.publicKey());
-  //           expect(firstTransaction.tokentype_uuid).toBe(tokenType.uuid);
-  //           expect(secondTransaction.tokentype_uuid).toBe(tokenType.uuid);
-  //           done();
-  //         };
-  //         await transactions.provenanceChain({
-  //         params: {
-  //           transaction_uuid: newTransaction.uuid
-  //         }
-  //       }, new psuedoRes(tests));
-  //     };
-  //     createChildTransaction(receiverKeypair, transaction.txn.dataValues.uuid, tokenType.uuid, AMOUNT, tHandler);
-  //   });
-  // });
+  describe('provenanceChain', () => {
+    it('returns an accurate provenance chain', async (done) => {
+      let receiverKeypair1;
+      let receiverKeypair2;
+      const handleShare1 = async sharedTransaction => {
+        receiverKeypair1 = sharedTransaction.receiverKeypair;
+        const transaction2 = sharedTransaction.transaction.txn;
+        shareTransaction(receiverKeypair1, transaction2.uuid, handleShare2);
+      };
+      const handleShare2 = async sharedTransaction => {
+        receiverKeypair2 = sharedTransaction.receiverKeypair;
+        const transaction3 = sharedTransaction.transaction.txn;
+        await transactions.provenanceChain({
+            params: { transactionUuid: transaction3.uuid}
+          }, new psuedoRes(handleProvenanceChain)
+        );
+      }
+      const handleProvenanceChain = (provenanceChain) => {
+        const firstTransaction = provenanceChain[0];
+        const secondTransaction = provenanceChain[1];
+        const thirdTransaction = provenanceChain[2];
+        expect(provenanceChain.length).toBe(3);
+        expect(firstTransaction.fromAddress).toBe(firstTransaction.toAddress);
+        expect(firstTransaction.amount).toBe(secondTransaction.amount);
+        expect(firstTransaction.amount).toBe(thirdTransaction.amount);
+        done();
+      };
+      shareTransaction(walletOwnerKeypair, transaction.uuid, handleShare1);
+    });
+  });
 
-  // describe('oldestProvenanceChain', () => {
-  //   it('returns an accurate provenance chain', async (done) => {
-  //     const tHandler = async (transactionObject) => {
-  //       const newTransaction = transactionObject.transaction.txn;
-  //       const newReceiverKeypair = transactionObject.receiverKeypair;
-  //       const tests = (provenanceChain) => {
-  //         expect(provenanceChain.length).toBe(2);
-  //         const firstTransaction = provenanceChain[0];
-  //         const secondTransaction = provenanceChain[1];
-  //         expect(secondTransaction.uuid).toBe(newTransaction.uuid);
-  //         expect(firstTransaction.amount).toBe(AMOUNT);
-  //         expect(secondTransaction.amount).toBe(AMOUNT);
-  //         expect(firstTransaction.fromAddress).toBe(walletOwnerKeypair.publicKey());
-  //         expect(firstTransaction.toAddress).toBe(receiverKeypair.publicKey());
-  //         expect(secondTransaction.fromAddress).toBe(receiverKeypair.publicKey());
-  //         expect(secondTransaction.toAddress).toBe(newReceiverKeypair.publicKey());
-  //         expect(firstTransaction.tokentype_uuid).toBe(tokenType.uuid);
-  //         expect(secondTransaction.tokentype_uuid).toBe(tokenType.uuid);
-  //         done();
-  //       };
-  //       await transactions.oldestProvenanceChain({
-  //         params: {
-  //           wallet_uuid: newReceiverKeypair.publicKey(),
-  //           tokentype_uuid: tokenType.uuid
-  //         }
-  //       }, new psuedoRes(tests));
-  //     };
-  //     createChildTransaction(receiverKeypair, transaction.txn.dataValues.uuid, tokenType.uuid, AMOUNT, tHandler);
-  //   });
-  //   it('throws an error when given an invalid token_uuid', async (done) => {
-  //     const tests = (res) => {
-  //       expect(res.message).not.toBe(undefined);
-  //       done();
-  //     };
-  //     await transactions.oldestProvenanceChain({
-  //       params: {
-  //         wallet_uuid: walletOwnerKeypair.publicKey(),
-  //         tokentype_uuid: '44444444-4444-4444-4444-444444444444'
-  //       }
-  //     }, new psuedoRes(tests));
-  //   });
-  //   it('throws an error when given an invalid wallet_uuid', async (done) => {
-  //     const tests = (res) => {
-  //       expect(res.message).not.toBe(undefined);
-  //       done();
-  //     };
-  //     await transactions.oldestProvenanceChain({
-  //       params: {
-  //         wallet_uuid: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-  //         tokentype_uuid: tokenType.uuid
-  //       }
-  //     }, new psuedoRes(tests));
-  //   });
-  // });
+  describe('provenanceChainFIFO', () => {
+    it('returns an accurate provenance chain', async (done) => {
+      let receiverKeypair1;
+      let receiverKeypair2;
+      const handleShare1 = async sharedTransaction => {
+        receiverKeypair1 = sharedTransaction.receiverKeypair;
+        const transaction2 = sharedTransaction.transaction.txn;
+        shareTransaction(receiverKeypair1, transaction2.uuid, handleShare2);
+      };
+      const handleShare2 = async sharedTransaction => {
+        receiverKeypair2 = sharedTransaction.receiverKeypair;
+        await transactions.provenanceChainFIFO({
+          params: {
+            walletUuid: receiverKeypair2.publicKey(),
+            tokenTypeUuid: tokenType.uuid
+          }
+        }, new psuedoRes(handleProvenanceChain)
+        );
+      }
+      const handleProvenanceChain = (provenanceChain) => {
+        const firstTransaction = provenanceChain[0];
+        const secondTransaction = provenanceChain[1];
+        const thirdTransaction = provenanceChain[2];
+        expect(provenanceChain.length).toBe(3);
+        expect(firstTransaction.fromAddress).toBe(firstTransaction.toAddress);
+        expect(firstTransaction.amount).toBe(secondTransaction.amount);
+        expect(firstTransaction.amount).toBe(thirdTransaction.amount);
+        done();
+      };
+      shareTransaction(walletOwnerKeypair, transaction.uuid, handleShare1);
+    });
+    it('throws an error when given an invalid token_uuid', async (done) => {
+      const tests = (res) => {
+        expect(res.message).not.toBe(undefined);
+        done();
+      };
+      await transactions.provenanceChainFIFO({
+        params: {
+          walletUuid: walletOwnerKeypair.publicKey(),
+          tokentypeUuid: '44444444-4444-4444-4444-444444444444'
+        }
+      }, new psuedoRes(tests));
+    });
+    it('throws an error when given an invalid wallet_uuid', async (done) => {
+      const tests = (res) => {
+        expect(res.message).not.toBe(undefined);
+        done();
+      };
+      await transactions.provenanceChainFIFO({
+        params: {
+          walletUuid: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+          tokentypeUuid: tokenType.uuid
+        }
+      }, new psuedoRes(tests));
+    });
+  });
 });
