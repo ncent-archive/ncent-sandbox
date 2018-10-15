d8e03097e46dc46c9d9464ff20ac1c8e54e0f4c9
Adam Foosaner <nucat176@gmail.com>
2018-10-15 14:25:16 -0700

    Adds challenge migration and model to sandbox

---
+++ server/migrations/20181015211140-create-challenge.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Challenges', {
+      uuid: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      expiration: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Challenges');
+  }
+};
